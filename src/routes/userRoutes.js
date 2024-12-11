d085a4b0262fb933255b40cda5261dfe6ea29009
Abdullah Al Maksud <maksud.workspace@gmail.com>
2024-12-11 22:34:15 +0600

    role based access system integreded

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const verifyToken = require("../middlewares/authMiddleware");
+const authorizedRoles = require("../middlewares/roleMiddleware");
 const router = express.Router();
 
 //Only admin can access this route
-router.get("/admin", verifyToken, (req, res) => {
+router.get("/admin", verifyToken, authorizedRoles("admin"), (req, res) => {
   res.json({ message: "Welcome to the admin" });
 });
 //Only user can access this route
-router.get("/manager", verifyToken, (req, res) => {
-  res.json({ message: "Welcome to the Manager" });
-});
+router.get(
+  "/user",
+  verifyToken,
+  authorizedRoles("admin", "user"),
+  (req, res) => {
+    res.json({ message: "Welcome to the Manager" });
+  }
+);
 
 //All can access this route
-router.get("/user", verifyToken, (req, res) => {
+router.get("/all", verifyToken, (req, res) => {
   res.json({ message: "Welcome to the User" });
 });
 
